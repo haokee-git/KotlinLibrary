2f211d5eb1d86b549535748c01d0cece534872ba
haokee-git <146501686+haokee-git@users.noreply.github.com>
2024-04-15 22:16:25 +0800

    2024.4.15 更新

---
+++ lib/BinaryIndexedTree.kt
@@ -0,0 +1,37 @@
+import java.util.*
+
+class BinaryIndexedTree<Ty>(private val size: Int,
+                            private val def: Ty,
+                            private val function: (Ty, Ty) -> Ty) {
+  private val tr = ArrayList<Ty>(size + 1)
+
+  init {
+    for (i in 0..size) {
+      tr.add(def)
+    }
+  }
+
+  private fun getLowBit(x: Int): Int {
+    return x and -x
+  }
+
+  fun modify(x: Int, y: Ty) {
+    assert(x in 1..size)
+    var i = x
+    while (i < size) {
+      tr[i] = function(tr[i], y)
+      i += getLowBit(i)
+    }
+  }
+
+  fun query(x: Int): Ty {
+    assert(x in 1..size)
+    var i = x
+    var res = def
+    while (i != 0) {
+      res = function(res, tr[i])
+      i -= getLowBit(i)
+    }
+    return res
+  }
+}
